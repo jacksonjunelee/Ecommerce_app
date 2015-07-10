480328ab4e35c18afdf1289d03bbbeb9d5cc6964
jjlee5266 <jjlee5266@gmail.com>
2015-07-09 21:10:50 -0400

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create([{name: 'Motorcycle', price: 43.20, quantity: 5, description: "rad Motorcycle"}])
-Product.create([{name: 'Bike', price: 42.10, quantity: 10, description: "4 wheel bike"}])
-Product.create([{name: 'Car', price: 22.20, quantity: 0, description: "Cool Car"}])
+Product.create([{name: 'Motorcycle', price: 43.20, quantity: 5, description: "rad Motorcycle", url: "http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg"}])
+Product.create([{name: 'Bike', price: 42.10, quantity: 10, description: "4 wheel bike", url: "http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg"}])
+Product.create([{name: 'Car', price: 22.20, quantity: 0, description: "Cool Car", url: "http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg"}])
 # User.create([{username: 'Moo', firstname: 'JAckson', lastname: 'Lee'}])
