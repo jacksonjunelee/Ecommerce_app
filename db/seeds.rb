e996c4e1d2061a5c017346da70334569cf466673
jjlee5266 <jjlee5266@gmail.com>
2015-07-09 20:59:34 -0400

    fix layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Product.create([{name: 'Motorcycle', price: 43.20, quantity: 5, description: "rad Motorcycle"}])
-# Product.create([{name: 'Bike', price: 42.10, quantity: 10, description: "4 wheel bike"}])
-# Product.create([{name: 'Car', price: 22.20, quantity: 0, description: "Cool Car"}])
+Product.create([{name: 'Motorcycle', price: 43.20, quantity: 5, description: "rad Motorcycle"}])
+Product.create([{name: 'Bike', price: 42.10, quantity: 10, description: "4 wheel bike"}])
+Product.create([{name: 'Car', price: 22.20, quantity: 0, description: "Cool Car"}])
 # User.create([{username: 'Moo', firstname: 'JAckson', lastname: 'Lee'}])
